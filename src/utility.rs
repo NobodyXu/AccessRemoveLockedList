c7cedc36e55a240b921ea8cfc8d8dd83c5475516
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-30 23:06:27 +1000

    Impl new func utility::assert_store_ptr_relaxed
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/utility.rs
+++ src/utility.rs
@@ -7,3 +7,13 @@ pub const W_ORD: Ordering = Ordering::Release;
 pub fn assert_store_ptr<T>(atomic: &AtomicPtr<T>, old_val: *mut T, new_val: *mut T) {
     atomic::assert_store_ptr(atomic, old_val, new_val, RW_ORD, W_ORD);
 }
+
+pub fn assert_store_ptr_relaxed<T>(
+    atomic: &AtomicPtr<T>,
+    old_val: *mut T,
+    new_val: *mut T
+) {
+    use Ordering::Relaxed;
+
+    atomic::assert_store_ptr(atomic, old_val, new_val, Relaxed, Relaxed);
+}
