289db9cd94b7830c55302435a9d0347ef5dd41d9
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-31 17:54:26 +1000

    Bump dep concurrency_toolkit version for new fix
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,3 @@ pub extern crate concurrency_toolkit;
 mod utility;
 pub mod intrusive_forward_list;
 pub mod intrusive_list;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
