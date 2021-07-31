fa71c7ecde0f7335f5e3d87717e563cae8ea0af0
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-31 17:54:50 +1000

    Add test.sh for conveniently executing tests with diff features
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -ex
+
+for feature in default permutation_testing; do
+    cargo test --no-default-features --features $feature
+    cargo test --release --no-default-features --features $feature
+done
