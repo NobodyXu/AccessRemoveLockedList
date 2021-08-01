d47639c3b0836d373c33006d4d609792583dc0dc
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-01 22:43:56 +1000

    `export RUST_BACKTRACE=1` in test.sh before running any test
    So on failure, a traceback will be shown automatically
    
    The previous commit didn't actually add this xD
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -ex
 
+export RUST_BACKTRACE=1
+
 for feature in default permutation_testing; do
     cargo test --no-default-features --features $feature $@
     cargo test --release --no-default-features --features $feature $@
