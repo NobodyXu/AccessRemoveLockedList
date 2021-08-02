376079b57b876a65fd0ec505db152a054087baaf
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-02 18:44:05 +1000

    Update /test.sh to use miri if possible
    Due to the fact that loom uses syscall not supported by miri, it cannot
    be used to run test when feature `permuation_testing` is enabled.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -1,8 +1,19 @@
 #!/bin/sh -ex
 
-export RUST_BACKTRACE=1
+cd $(dirname $(realpath $0))
 
-for feature in default permutation_testing; do
-    cargo test --no-default-features --features $feature $@
-    cargo test --release --no-default-features --features $feature $@
+export RUST_BACKTRACE=1
+# Allow environment variables to pass through
+export MIRIFLAGS="-Zmiri-disable-isolation"
+
+args="--no-default-features"
+miri_args="$args --target-dir miri-target"
+
+for feature in default; do
+    cargo +nightly miri test $miri_args --features $feature $@
+    cargo +nightly miri test --release $miri_args --features $feature $@
 done
+
+feature="permutation_testing"
+cargo test $args --features $feature $@
+cargo test --release $args --features $feature $@
