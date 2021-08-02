238fece4418d0ad26ab0dd91dcc5c8588caf75c3
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-02 22:54:13 +1000

    Clean up env var before running tests in test.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -1,19 +1,26 @@
 #!/bin/sh -ex
 
-cd $(dirname $(realpath $0))
+main() {
+    export RUST_BACKTRACE=1
+    # Allow environment variables to pass through
+    export MIRIFLAGS="-Zmiri-disable-isolation"
+    
+    args="--no-default-features"
+    miri_args="$args --target-dir miri-target"
+    
+    for feature in default; do
+        cargo +nightly miri test $miri_args --features $feature $@
+        cargo +nightly miri test --release $miri_args --features $feature $@
+    done
+    
+    feature="permutation_testing"
+    cargo test $args --features $feature $@
+    cargo test --release $args --features $feature $@
+}
 
-export RUST_BACKTRACE=1
-# Allow environment variables to pass through
-export MIRIFLAGS="-Zmiri-disable-isolation"
-
-args="--no-default-features"
-miri_args="$args --target-dir miri-target"
-
-for feature in default; do
-    cargo +nightly miri test $miri_args --features $feature $@
-    cargo +nightly miri test --release $miri_args --features $feature $@
-done
-
-feature="permutation_testing"
-cargo test $args --features $feature $@
-cargo test --release $args --features $feature $@
+if [ -z ${CLEARED+x} ]; then
+    exec env -i CLEARED=1 PATH="$PATH" $0 $@
+else
+    cd $(dirname $(realpath $0))
+    main
+fi
