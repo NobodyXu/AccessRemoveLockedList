70f75113c60f9c61807fcec24a4a69b3e494ff82
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-03 13:53:10 +1000

    Run permutation_testing before miri
    For it is way faster to run and I would like to configure it in future
    commits to give a much better explanation of the failure
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -5,18 +5,18 @@ main() {
     # Allow environment variables to pass through
     export MIRIFLAGS="-Zmiri-disable-isolation"
     export LANG=C.UTF8
-    
+
     args="--no-default-features"
     miri_args="$args --target-dir miri-target"
-    
+
+    feature="permutation_testing"
+    cargo test $args --features $feature $@
+    cargo test --release $args --features $feature $@
+
     for feature in default; do
         cargo +nightly miri test $miri_args --features $feature $@
         cargo +nightly miri test --release $miri_args --features $feature $@
     done
-    
-    feature="permutation_testing"
-    cargo test $args --features $feature $@
-    cargo test --release $args --features $feature $@
 }
 
 if [ -z ${CLEARED+x} ]; then
