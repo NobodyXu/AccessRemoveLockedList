a2ed3a162cdbed99838c1f042912c10c8b3355f0
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-03 14:01:47 +1000

    Configure loom to print errors/logs to stderr/file
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- test.sh
+++ test.sh
@@ -6,6 +6,12 @@ main() {
     export MIRIFLAGS="-Zmiri-disable-isolation"
     export LANG=C.UTF8
 
+    export LOOM_CHECKPOINT_FILE=target/loom_test.json
+    export LOOM_CHECKPOINT_INTERVAL=1
+    export LOOM_LOG=1
+    export LOOM_LOCATION=1
+    export LOOM_MAX_PREEMPTIONS=10
+
     args="--no-default-features"
     miri_args="$args --target-dir miri-target"
 
