2f02172420c99d12738c73cfb87dee791b37ceb8
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-03 14:46:05 +1000

    Add script debug_loom_test.sh
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

---
+++ debug_loom_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export LOOM_CHECKPOINT_FILE=loom.json
+export LOOM_CHECKPOINT_INTERVAL=1
+export LOOM_LOG=1
+export LOOM_LOCATION=1
+
+feature="permutation_testing"
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <test name> [args to cargo test]" >&2
+    exit 1
+fi
+
+exec cargo test \
+    --target-dir target-"$feature" \
+    --release \
+    --no-default-features \
+    --features "$feature" \
+    --test $1 \
+    ${@:2}
